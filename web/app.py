6761b6fcc2f415cd96ee1d26d5d6edf7ade48345
John Mack <mackspace@gmail.com>
2020-03-14 20:14:31 -0600

    added some styling

--- web/app.py
+++ web/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, redirect, request, session
 import requests, base64
-from access import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+from access import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SECRET_KEY
 from forms import SearchForm
 from utils import get_id, get_genres, get_key_list, get_modes
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect()
 
 app = Flask(__name__)
-app.secret_key = "password"
+app.config['SECRET_KEY'] = SECRET_KEY
+
+csrf.init_app(app)
 
 @app.before_request
 def log_request_info():
@@ -70,6 +75,8 @@ def search_spotify():
 @app.route("/search/seed", methods=['POST'])
 def show_recommendations():
     """"""
+
+
 
     headers = {'Authorization':'Bearer ' + session['token']}
     
@@ -141,53 +148,3 @@ def show_recommendations():
 
 # https://api.spotify.com/v1/recommendations
 # http://127.0.0.1:5000
-
-
-
-# limit
-# market
-# seed_artists
-# seed_genres
-# seed_tracks
-# min_acousticness
-# max_acousticness
-# target_acousticness
-# min_danceability
-# max_danceability
-# target_danceability
-# min_duration
-# max_duration
-# target_duration
-# min_energy
-# max_energy
-# target_energy
-# min_instrumentalness
-# max_instrumentalness
-# arget_instrumentalness
-# min_key
-# max_key
-# target_key
-# min_liveness
-# max_liveness
-# target_liveness
-# min_loudness
-# max_loudness
-# target_loudness
-# min_mode
-# max_mode
-# target_mode
-# min_popularity
-# max_popularity
-# target_popularity
-# min_speechiness
-# max_speechiness
-# target_speechiness
-# min_tempo
-# max_tempo
-# target_tempo
-# min_time_signature
-# max_time_signature
-# target_time_signature
-# min_valence
-# max_valence
-# target_valence
--- web/forms.py
+++ web/forms.py
@@ -6,18 +6,18 @@ class SearchForm(FlaskForm):
     """"""
     
 
-    input1 = StringField("Artist or Song (optional)")
+    input1 = StringField(render_kw={"placeholder": "Optional artist or song"})
     radio1 = RadioField(choices=[('artist','Artist'),('track','Song')], validators=(validators.Optional(),))
 
-    input2 = StringField("Artist or Song (optional)")
+    input2 = StringField(render_kw={"placeholder": "Optional artist or song"})
     radio2 = RadioField(choices=[('artist','Artist'),('track','Song')], validators=(validators.Optional(),))
 
-    input3 = StringField("Artist or Song (optional)")
+    input3 = StringField(render_kw={"placeholder": "Optional artist or song"})
     radio3 = RadioField(choices=[('artist','Artist'),('track','Song')], validators=(validators.Optional(),))
 
-    input4 = StringField("Artist or Song (optional)")
+    input4 = StringField(render_kw={"placeholder": "Optional artist or song"})
     radio4 = RadioField(choices=[('artist','Artist'),('track','Song')], validators=(validators.Optional(),))
 
-    input5 = StringField("Artist or Song (optional)")
+    input5 = StringField(render_kw={"placeholder": "Optional artist or song"})
     radio5 = RadioField(choices=[('artist','Artist'),('track','Song')], validators=(validators.Optional(),))
     
